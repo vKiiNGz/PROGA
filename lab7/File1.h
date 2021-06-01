0e8954a762e8f513c92dbabe2cf17e6385c09849
vKiiNGz <76061721+vKiiNGz@users.noreply.github.com>
2021-06-01 12:03:51 +0300

    Update File1.h

--- lab7/File1.h
+++ lab7/File1.h
@@ -9,11 +9,10 @@ using namespace std;
 void StarLine();
 string DateIn(bool ff);
 string TimeIn();
-void AddContact(Person &user);
-void AddEvent(Person &user);
 string SearchContacnt(Person &user, string _name, string _surname);
 string Schedule(Person &user, string _date);
 void In(Person &user, fstream &baze);
 void Out(Person &user, fstream &baze);
 string* ShowAllContacts(Person &user);
 string* ShowAllEvents(Person &user);
+void start(Person &user , fstream &baze, string &name, string &surname)
